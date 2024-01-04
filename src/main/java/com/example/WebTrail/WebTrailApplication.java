5257767bf9df53f6d3f858ad7f596e4b9b4b4244
puja0709 <102405052+puja0709@users.noreply.github.com>
2024-01-04 14:50:02 +0530

    exam

---
+++ src/main/java/com/example/WebTrail/WebTrailApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebTrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WebTrail/WebTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebTrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
